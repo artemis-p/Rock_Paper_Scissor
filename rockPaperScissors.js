1e2d9e19f7fb1ab56ff75963b8b5a9f86c8ebb7c
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-09-16 10:07:32 +0100

    Get computer's choice and results logic, amended colours in CSS, removed div from HTML

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,54 +1,94 @@
-let getComputerChoice;
-let getUserChoice;
-let result = results();
+let userChosen;
+let result = getResults();
+const displayResult = document.getElementById('result')
+const choices = document.querySelectorAll('.choices');
+const randomNumber = Math.floor(Math.random() * 5);
+const userChoice = document.getElementById('.name-0')
 
+
+//get user's choice
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        getUserChoice = e.target.id
+
+    })
+})
+
+
+//get computer's choice
+function getComputerChoice() {
+    switch (randomNumber){
+        case 0:
+            return("rock");
+        case 1:
+            return("paper");
+        case 2:
+            return("scissors");
+        case 3:
+            return("lizard");
+        case 4:
+            return("spock");
+    }
+}
+console.log(getComputerChoice());
 
 //get Results
-function results() {
-    if (getComputerChoice === getUserChoice) {
+function getResults() {
+    if (getComputerChoice === userChosen) {
         return result = "It's a tie!"
-    } else if (getComputerChoice === "scissors" && getUserChoice === "paper") {
+    } else if (getComputerChoice === "scissors" && userChosen === "paper") {
         return result = "You lost!"
-    } else if (getComputerChoice === "scissors" && getUserChoice === "rock") {
+    } else if (getComputerChoice === "scissors" && userChosen === "rock") {
         return result = "You win!"
-    } else if (getComputerChoice === "scissors" && getUserChoice === "lizard") {
+    } else if (getComputerChoice === "scissors" && userChosen === "lizard") {
         return result = "You lost!"
-    } else if (getComputerChoice === "scissors" && getUserChoice === "spock") {
+    } else if (getComputerChoice === "scissors" && userChosen === "spock") {
         return result = "You win!"
-    } else if (getComputerChoice === "rock" && getUserChoice === "paper") {
+    } else if (getComputerChoice === "rock" && userChosen === "paper") {
         return result = "You win!"
-    } else if (getComputerChoice === "rock" && getUserChoice === "scissors") {
+    } else if (getComputerChoice === "rock" && userChosen === "scissors") {
         return result = "You lost!"
-    } else if (getComputerChoice === "rock" && getUserChoice === "lizard") {
+    } else if (getComputerChoice === "rock" && userChosen === "lizard") {
         return result = "You lost!"
-    } else if (getComputerChoice === "rock" && getUserChoice === "spock") {
+    } else if (getComputerChoice === "rock" && userChosen === "spock") {
         return result = "You win!"
-    } else if (getComputerChoice === "paper" && getUserChoice === "rock") {
+    } else if (getComputerChoice === "paper" && userChosen === "rock") {
         return result = "You lost!"
-    } else if (getComputerChoice === "paper" && getUserChoice === "scissors") {
+    } else if (getComputerChoice === "paper" && userChosen === "scissors") {
         return result = "You win!"
-    } else if (getComputerChoice === "paper" && getUserChoice === "lizard") {
+    } else if (getComputerChoice === "paper" && userChosen === "lizard") {
         return result = "You win!"
-    } else if (getComputerChoice === "paper" && getUserChoice === "spock") {
+    } else if (getComputerChoice === "paper" && userChosen === "spock") {
         return result = "You lost!"
-    } else if (getComputerChoice === "paper" && getUserChoice === "spock") {
+    } else if (getComputerChoice === "paper" && userChosen === "spock") {
         return result = "You lost!"
-    } else if (getComputerChoice === "lizard" && getUserChoice === "rock") {
+    } else if (getComputerChoice === "lizard" && userChosen === "rock") {
         return result = "You win!"
-    } else if (getComputerChoice === "lizard" && getUserChoice === "paper") {
+    } else if (getComputerChoice === "lizard" && userChosen === "paper") {
         return result = "You lost!"
-    } else if (getComputerChoice === "lizard" && getUserChoice === "scissors") {
+    } else if (getComputerChoice === "lizard" && userChosen === "scissors") {
         return result = "You win!"
-    } else if (getComputerChoice === "lizard" && getUserChoice === "spock") {
+    } else if (getComputerChoice === "lizard" && userChosen === "spock") {
         return result = "You lost!"
-    } else if (getComputerChoice === "spock" && getUserChoice === "rock") {
+    } else if (getComputerChoice === "spock" && userChosen === "rock") {
         return result = "You lost!"
-    } else if (getComputerChoice === "spock" && getUserChoice === "paper") {
+    } else if (getComputerChoice === "spock" && userChosen === "paper") {
         return result = "You win!"
-    } else if (getComputerChoice === "spock" && getUserChoice === "scissors") {
+    } else if (getComputerChoice === "spock" && userChosen === "scissors") {
         return result = "You lost!"
-    } else if (getComputerChoice === "spock" && getUserChoice === "lizard") {
+    } else if (getComputerChoice === "spock" && userChosen === "lizard") {
         return result = "You win!"
     }
 }
 
+
+
+
+// Play the game
+
+//the user clicks on a button (r,p,s,l,s) - userChosen
+//save this in a var - userChosen
+//the computer plays a random choice - getComputerChoice - yes
+//save this in a var - computerChosen
+// compare the two var and decide the winner - getResults getComputerChoice vs userChosen - yes
+// print result you win/ you lose - displayResult
